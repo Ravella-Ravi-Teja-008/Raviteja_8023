64d53e670fd0768f8f0c5e149989cf3f614d5a19
ravella-raviteja <7893388365@gitam.in>
2021-07-02 10:15:36 +0530

    done

---
+++ binarysearch.py
@@ -0,0 +1,19 @@
+def binary_search(arr,low,high,x):
+    if high>low:
+        mid=(high+low)//2
+        if arr[mid]==x:
+            return mid
+        elif mid>x:
+            return binary_search(arr,low,mid-1,x)
+        elif mid<x:
+            return binary_search(arr,mid+1,high,x)
+    else:
+        return -1
+        
+        
+arr=[20,26,45,56,454,465,988,1000]
+result=binary_search(arr,0,len(arr)-1,988)
+if result !=-1:
+    print("found",result)
+else:
+    print("not found")
