1e77116b72784b6d349c14aac13b1885a4b504ef
ravella-raviteja <7893388365@gitam.in>
2021-07-02 10:13:49 +0530

    done

---
+++ linear search.py
@@ -0,0 +1,13 @@
+def search(arr, x):
+    for i in range(len(arr)):
+        if arr[i] == x:
+            return i
+  
+    return -1
+    
+arr=[1,252,43,34,87,45,76]
+result=search(arr,87)
+if result !=-1:
+    print('element is found',result)
+else:
+    print("ele not found")
