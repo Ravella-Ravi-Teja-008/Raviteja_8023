416427456c147eed3f83b51525e2e7a056fabdd8
ravella-raviteja <7893388365@gitam.in>
2021-07-22 18:37:21 +0530

    done

--- bubblesort.py
+++ bubblesort.py
@@ -1,21 +1,25 @@
+print("Raviteja 222010308023")
 a=[]
-p=int(input('enter no. of values'))
+p=int(input('enter no. of values '))
 for k in range(p):
-    e=int(input('enter the number'))
+    e=int(input('enter the number '))
     a.append(e)
 
 
-n=len(a)
-for i in range(n-1):
+count=0
+for i in range(p-1):
     flag=0
 
-    for j in range(n-1):
+    for j in range(p-1):
+        count+=1
         if a[j]>a[j+1]:
             emp=a[j]
             a[j]=a[j+1]
+            a[j+1]=emp
             flag=1
 
     if flag==0:
         break
 print(a)
+print("NO. of comparision ",count)
     
